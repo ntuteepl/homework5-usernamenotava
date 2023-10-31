83cf753f96e2e6da9f56bdfaefe764a3ce20aebd
usernamenotava <146065210+usernamenotava@users.noreply.github.com>
2023-10-31 15:29:55 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,25 @@
-
+#include <stdio.h>
+int main()
+{
+	float a;
+	float b;
+	scanf("%f",&a);
+if(a<=800){
+	b=a*0.9;
+	double db;
+	printf("%.1f",b);
+}else{
+	if(a>800 && a<=1500){
+		b=a*0.9*0.9;
+		double db;
+		printf("%.1f",b);
+	}else{
+		if(a>1500){
+			b=a*0.9*0.79;
+			double db;
+			printf("%.1f",b);
+		}
+	}
+}
+}
+  
